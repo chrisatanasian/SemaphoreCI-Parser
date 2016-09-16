bf81b9ad3b164cb1ab12adf248cd7f05243c2166
Chris Atanasian <catanasian@gmail.com>
2016-09-16 13:32:05 -0700

    Get parser to parse a local file and print out build totals

---
+++ semaphore_parser.rb
@@ -0,0 +1,49 @@
+require 'nokogiri'
+require 'pry'
+
+def open_file(filename)
+  Nokogiri::HTML(open(filename))
+end
+
+def semaphore_thread_outputs(semaphore_log_file)
+  semaphore_log_file.css(".c-results_list_pre.ng-isolate-scope").map do |thread|
+    thread.text.scan(/\d+ tests, \d+ assertions, \d+ failures, \d+ errors, \d+ skips/)
+  end.select.with_index { |_, i| i.odd? }
+end
+
+def thread_line(thread_number, thread_output)
+  "#{thread_number}: #{thread_output.empty? ? "Passed" : thread_output.join("\n    ")}"
+end
+
+def print_build_information(semaphore_log_file)
+  build_number = semaphore_log_file.css(".c-build-meta_list_status.btn-group").text.scan(/\d+/).first
+end
+
+def print_totals(thread_outputs)
+  totals = { tests: 0, assertions: 0, failures: 0, errors: 0, skips: 0 }
+
+  thread_outputs.each_with_index do |thread_output, i|
+    thread_output.map do |single_thread_output|
+      single_thread_output_totals = single_thread_output.scan(/\d+/)
+
+      totals.each_with_index do |(key, _), i|
+        totals[key] += single_thread_output_totals[i].to_i
+      end
+    end
+
+    puts thread_line(i + 1, thread_output)
+  end
+
+  puts ""
+
+  totals.each do |key, value|
+    puts "#{value} #{key}"
+  end
+end
+
+semaphore_log_file = open_file("semaphore.html")
+
+print_build_information(semaphore_log_file)
+
+thread_outputs     = semaphore_thread_outputs(semaphore_log_file)
+print_totals(thread_outputs)
